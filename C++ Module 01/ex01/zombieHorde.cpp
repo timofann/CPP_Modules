a148143084b18fa8d16b0d94a0161ad88dd7620a
dedelmir <anytat@inbox.ru>
2022-08-03 01:55:44 +0300

    negative zombies count fix

--- C++ Module 01/ex01/zombieHorde.cpp
+++ C++ Module 01/ex01/zombieHorde.cpp
@@ -1,6 +1,10 @@
 #include "Zombie.h"
 
 Zombie*	zombieHorde(int n, std::string _name) {
+	if (n < 0) {
+		std::cout << "Need more zombieeezzzzzZ" << std::endl;
+		return NULL;
+	}
 	try {
 		Zombie* horde = new Zombie[n];
 		for (int i = 0; i < n; i++) {
