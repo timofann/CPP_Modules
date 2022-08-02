857de848c93eee86b2822bcdfdef92bc516f9521
dedelmir <anytat@inbox.ru>
2022-08-02 22:13:16 +0300

    Constructors and destructor for Zombie

--- C++ Module 01/ex00/Zombie.cpp
+++ C++ Module 01/ex00/Zombie.cpp
@@ -1,7 +1,25 @@
 #include "Zombie.h"
+#include <iostream>
+#include <string>
 
-Zombie::Zombie(void) {}
+Zombie::Zombie(void) {
+	std::cout << "\033[3;37m"
+			  << "Zombie default constructor called"
+			  << "\033[0m" << std::endl << std::endl;
+}
 
-Zombie::Zombie(std::string _name) {}
+Zombie::Zombie(std::string _name) {
+	this->name = _name;
 
-Zombie::~Zombie(void) {}
+	std::cout << "\033[3;37m"
+			  << "Zombie constructor called for the next instance:"
+			  << "\033[0m" << std::endl;
+	std::cout << "ZombieName: " << this->name << std::endl;
+}
+
+Zombie::~Zombie(void) {
+	std::cout << "\033[3;37m"
+			  << "Zombie destructor called for the next instance:"
+			  << "\033[0m" << std::endl;
+	std::cout << "ZombieName: " << this->name << std::endl;
+}
