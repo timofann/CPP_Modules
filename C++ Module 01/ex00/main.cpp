0d7788a5abfc56bf1d9c3eb97128c291b8b95db9
dedelmir <anytat@inbox.ru>
2022-08-03 00:56:14 +0300

    main.cpp file with examples added

--- C++ Module 01/ex00/main.cpp
+++ C++ Module 01/ex00/main.cpp
@@ -1,8 +1,19 @@
 #include "Zombie.h"
-#include <iostream>
 
 int	main(void) {
 	Zombie my_first_zombie;
-
 	my_first_zombie = Zombie("FirstZombiePoul");
+	my_first_zombie.announce();
+
+	Zombie my_second_zombie = Zombie("SecondZombieJacob");
+	my_second_zombie.announce();
+
+	randomChump("ThirdZombieSousanna");
+
+	Zombie my_fourth_zombie("FourthZombieTom");
+	my_fourth_zombie.announce();
+
+	Zombie *my_fifth_zombie = newZombie("FifthZombieKate");
+	my_fifth_zombie->announce();
+	delete my_fifth_zombie;
 }
