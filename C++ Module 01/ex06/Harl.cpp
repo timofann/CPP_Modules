6a7a7192a328bb5297834c8ca0af6ec6cbdc08c5
dedelmir <anytat@inbox.ru>
2022-08-06 14:18:31 +0300

    pretty output for Harl (M01 ex05 ex06)

--- C++ Module 01/ex06/Harl.cpp
+++ C++ Module 01/ex06/Harl.cpp
@@ -19,6 +19,7 @@ void	Harl::complain(std::string level) {
 	size_t i;
 	for (i = 0; i < 4 && this->pairs[i].m_name != level; ++i) {};
 	if (i == 4) {
+		std::cout << "[EMPTY]" << std::endl;
 		std::cout << "There is no words in the world to "
 					 "describe this situation!" << std::endl;
 	}
